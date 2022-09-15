a2b0b6062e7c870b63f4a09bf6eff4932be5e35c
emericgautier <emericgtr@gmail.com>
2022-09-15 10:06:30 +0200

    Create Treemap

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
+import axios from "axios";
 import React from "react";
+import { useState, useEffect } from "react";
 import GlobalChart from "./components/GlobalChart";
 import HeaderInfos from "./components/HeaderInfos";
 
 const App = () => {
+  const [coinsData, setCoinsData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=false&price_change_percentage=1h%2C24h%2C7d%2C14d%2C30d%2C200d%2C1y"
+      )
+      .then((res) => setCoinsData(res.data));
+  }, []);
+
   return (
     <div className="app-container">
       <header>
         <HeaderInfos />
-        <GlobalChart />
+        <GlobalChart coinsData={coinsData} />
       </header>
     </div>
   );
