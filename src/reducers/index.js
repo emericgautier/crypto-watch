9c0ad751a77642e30f4e0ad5d635bd92680d7616
emericgautier <emericgtr@gmail.com>
2022-09-18 11:54:59 +0200

    using redux to filter

---
+++ src/reducers/index.js
@@ -0,0 +1,5 @@
+import { combineReducers } from "redux";
+import stableReducer from "./stable.reducer";
+import listReducer from "./list.reducer";
+
+export default combineReducers({ stableReducer, listReducer });
